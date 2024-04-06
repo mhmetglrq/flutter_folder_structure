86a4c999ffec06bd7b7b1029f1ccc6795d75afb5
mhmetglrq <mehmet.guler@ayssoft.com>
2024-04-06 17:23:59 +0300

    Add ImageEnum class for managing image assets

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_folder_structure/config/routes/app_router.dart';
+import 'package:flutter_folder_structure/config/routes/route_names.dart';
+import 'package:flutter_folder_structure/config/themes/app_theme.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    return MaterialApp(
+      theme: AppTheme.lightTheme,
+      home: const Scaffold(
         body: Center(
           child: Text('Hello World!'),
         ),
       ),
+      initialRoute: RouteNames.home,
+      onGenerateRoute: AppRouter.generateRoute,
     );
   }
 }
